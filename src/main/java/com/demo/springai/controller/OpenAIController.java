90f273b14fddd87271afaf6ca170cc4fd835d7c3
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-11 23:45:39 +0530

    create simple layer

--- src/main/java/com/demo/springai/controller/OpenAIController.java
+++ src/main/java/com/demo/springai/controller/OpenAIController.java
@@ -1,13 +1,21 @@
 package com.demo.springai.controller;
 
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAIController {
+
+    private final OpenAiChatModel chatModel;
+
+    public OpenAIController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
     @GetMapping("/api/{message}")
     public String getAnswer(@PathVariable String message){
-        return "Hello world " + message;
+        return chatModel.call(message);
     }
 }
