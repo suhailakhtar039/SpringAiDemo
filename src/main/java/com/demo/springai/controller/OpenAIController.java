492bc8eeba7900f2e3eb53163b9726015885b289
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-11 23:39:08 +0530

    dummy function created

--- src/main/java/com/demo/springai/controller/OpenAIController.java
+++ src/main/java/com/demo/springai/controller/OpenAIController.java
@@ -1,8 +1,13 @@
 package com.demo.springai.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAIController {
-
+    @GetMapping("/api/{message}")
+    public String getAnswer(@PathVariable String message){
+        return "Hello world " + message;
+    }
 }
