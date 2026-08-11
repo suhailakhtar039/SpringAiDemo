065f35d4059a9c0dcc012a1df0a4f34872ec302a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-11 23:52:53 +0530

    use custom status code and response

--- src/main/java/com/demo/springai/controller/OpenAIController.java
+++ src/main/java/com/demo/springai/controller/OpenAIController.java
@@ -1,6 +1,8 @@
 package com.demo.springai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenAIController {
 
-    private final OpenAiChatModel chatModel;
+    private final ChatClient chatClient;
 
     public OpenAIController(OpenAiChatModel chatModel) {
-        this.chatModel = chatModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping("/api/{message}")
-    public String getAnswer(@PathVariable String message){
-        return chatModel.call(message);
+    public ResponseEntity<String> getAnswer(@PathVariable String message) {
+        String response = chatClient
+                .prompt(message)
+                .call()
+                .content();
+        return ResponseEntity.ok(response);
     }
 }
