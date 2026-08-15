2f9d3005f57f7c47af86561a2ce5f9722278d850
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-15 23:45:49 +0530

    create bean explicitly

--- src/main/java/com/demo/springai/configuration/AppConfig.java
+++ src/main/java/com/demo/springai/configuration/AppConfig.java
@@ -3,16 +3,26 @@ package com.demo.springai.configuration;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public VectorStore vectorStore(@Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+    @Primary
+    public EmbeddingModel embeddingModel(
+            @Qualifier("openAiEmbeddingModel") EmbeddingModel openAiEmbeddingModel) {
+        return openAiEmbeddingModel;
+    }
+
+    @Bean
+    public VectorStore vectorStore(JdbcTemplate jdbcTemplate, @Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel).build();
     }
 
 }
