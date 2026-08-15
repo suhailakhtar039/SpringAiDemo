7e8c9f674a56063e3e1e104c11566548e615af59
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-15 21:05:13 +0530

    creating text splitter according to new version

--- src/main/java/com/demo/springai/component/DataInitializer.java
+++ src/main/java/com/demo/springai/component/DataInitializer.java
@@ -1,12 +1,35 @@
 package com.demo.springai.component;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class DataInitializer {
+
+    @Autowired
+    private VectorStore vectorStore;
+
+    @PostConstruct
     public void initData(){
         TextReader textReader = new TextReader(new ClassPathResource("product_details.txt"));
+        TokenTextSplitter splitter = TokenTextSplitter.builder()
+                .withChunkSize(500)
+                .withMinChunkSizeChars(30)
+                .withMinChunkLengthToEmbed(20)
+                .withMaxNumChunks(500)
+                .withKeepSeparator(false)
+                .build();
+
+        List<Document> documents = splitter.split(textReader.get());
+
+        vectorStore.add(documents);
     }
 }
