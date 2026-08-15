6de7a15a92d96f9ca6a28af402e967d963a94c0d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-15 13:28:13 +0530

    use embedding model

--- src/main/java/com/demo/springai/controller/OpenAIController.java
+++ src/main/java/com/demo/springai/controller/OpenAIController.java
@@ -1,23 +1,33 @@
 package com.demo.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAIController {
 
     private final ChatClient chatClient;
+
+    @Autowired
+    @Qualifier("openAiEmbeddingModel")
+    private EmbeddingModel embeddingModel;
+
     ChatMemory chatMemory = MessageWindowChatMemory
             .builder()
             .build();
+
     public OpenAIController(OpenAiChatModel chatModel) {
         this.chatClient = ChatClient.create(chatModel);
     }
@@ -52,5 +62,10 @@ public class OpenAIController {
                 .getText();
 
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/embedding")
+    public float[] embedding(@RequestParam String text) {
+        return embeddingModel.embed(text);
     }
 }
