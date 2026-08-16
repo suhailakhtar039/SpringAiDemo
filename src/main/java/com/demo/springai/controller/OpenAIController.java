0347dc8ee5398d7cd39444e1f256845840046c85
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-16 16:01:46 +0530

    rag implementation completed

--- src/main/java/com/demo/springai/controller/OpenAIController.java
+++ src/main/java/com/demo/springai/controller/OpenAIController.java
@@ -1,6 +1,7 @@
 package com.demo.springai.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -105,6 +106,9 @@ public class OpenAIController {
     public String getAnswerUsingReq(@RequestParam String query){
         return chatClient
                 .prompt(query)
+                .advisors(QuestionAnswerAdvisor.builder(vectorStore)
+                        .build()
+                )
                 .call()
                 .content();
     }
