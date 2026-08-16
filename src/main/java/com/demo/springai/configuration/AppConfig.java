f52b958010c717e545d0b6ea6f2be3c1d7d53527
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-16 15:30:49 +0530

    write only one function

--- src/main/java/com/demo/springai/configuration/AppConfig.java
+++ src/main/java/com/demo/springai/configuration/AppConfig.java
@@ -1,28 +1,41 @@
 package com.demo.springai.configuration;
 
 import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcTemplate;
+import redis.clients.jedis.RedisClient;
 
 @Configuration
 public class AppConfig {
 
+//    @Bean
+//    public RedisClient redisClient() {
+//        return RedisClient.builder()
+//                .hostAndPort("localhost", 6379)
+//                .build();
+//    }
+
     @Bean
     @Primary
     public EmbeddingModel embeddingModel(
-            @Qualifier("openAiEmbeddingModel") EmbeddingModel openAiEmbeddingModel) {
+            @Qualifier("openAiEmbeddingModel")
+            EmbeddingModel openAiEmbeddingModel) {
+
         return openAiEmbeddingModel;
     }
 
-    @Bean
-    public VectorStore vectorStore(JdbcTemplate jdbcTemplate, @Qualifier("openAiEmbeddingModel") EmbeddingModel embeddingModel) {
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel).build();
-    }
+//    @Bean
+//    public VectorStore vectorStore(
+//            RedisClient redisClient,
+//            @Qualifier("openAiEmbeddingModel")
+//            EmbeddingModel embeddingModel) {
+//
+//        return RedisVectorStore.builder(redisClient, embeddingModel)
+//                .build();
+//    }
 
 }
