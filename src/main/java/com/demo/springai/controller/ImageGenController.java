2c9d3668c45e117aca675be3fdcb916c4d0826c0
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-16 20:59:45 +0530

    skeleton created for ImageGenController

--- src/main/java/com/demo/springai/controller/ImageGenController.java
+++ src/main/java/com/demo/springai/controller/ImageGenController.java
@@ -1,7 +1,17 @@
 package com.demo.springai.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ImageGenController {
+    private ChatClient chatClient;
+    private OpenAiImageModel openAiImageModel;
+
+    public ImageGenController(OpenAiImageModel openAiImageModel, ChatClient.Builder builder){
+        this.openAiImageModel = openAiImageModel;
+        chatClient = builder.build();
+    }
+
 }
