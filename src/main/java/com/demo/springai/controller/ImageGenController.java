55525cc7fd21796a60008430111f685185af939b
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-18 22:56:56 +0530

    describing image

--- src/main/java/com/demo/springai/controller/ImageGenController.java
+++ src/main/java/com/demo/springai/controller/ImageGenController.java
@@ -2,16 +2,22 @@ package com.demo.springai.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Base64;
 
@@ -22,8 +28,11 @@ public class ImageGenController {
 
     private final OpenAiImageModel openAiImageModel;
 
-    public ImageGenController(OpenAiImageModel openAiImageModel) {
+    private ChatClient chatClient;
+
+    public ImageGenController(OpenAiImageModel openAiImageModel, OpenAiChatModel chatModel) {
         this.openAiImageModel = openAiImageModel;
+        this.chatClient = ChatClient.create(chatModel);
     }
 
     @GetMapping(value = "/image/{query}", produces = MediaType.IMAGE_PNG_VALUE)
@@ -57,5 +66,16 @@ public class ImageGenController {
             log.error("Image generation failed", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @PostMapping("/image/describe")
+    public String descImage(@RequestParam String query, @RequestParam MultipartFile file) {
+        return chatClient
+                .prompt()
+                .user(us -> us.text(query)
+                        .media(MimeTypeUtils.IMAGE_JPEG, file.getResource())
+                )
+                .call()
+                .content();
     }
 }
