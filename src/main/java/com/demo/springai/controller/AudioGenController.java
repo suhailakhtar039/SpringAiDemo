bbe0d3c6bb21cf18706035cd3df819010563f7bc
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-18 23:17:16 +0530

    structure for speech to text created

--- src/main/java/com/demo/springai/controller/AudioGenController.java
+++ src/main/java/com/demo/springai/controller/AudioGenController.java
@@ -1,7 +1,21 @@
 package com.demo.springai.controller;
 
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AudioGenController {
+
+    private OpenAiAudioTranscriptionModel audioModel;
+
+    public AudioGenController(OpenAiAudioTranscriptionModel audioModel) {
+        this.audioModel = audioModel;
+    }
+
+    @PostMapping("/api/stt")
+    public String speechToText() {
+
+    }
+
 }
