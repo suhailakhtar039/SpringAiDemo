54e88e7cd62677399ae181c64558d9ca7c6f7de9
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-18 23:36:54 +0530

    speech to text working

--- src/main/java/com/demo/springai/controller/AudioGenController.java
+++ src/main/java/com/demo/springai/controller/AudioGenController.java
@@ -1,8 +1,12 @@
 package com.demo.springai.controller;
 
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class AudioGenController {
@@ -14,8 +18,13 @@ public class AudioGenController {
     }
 
     @PostMapping("/api/stt")
-    public String speechToText() {
-
+    public String speechToText(@RequestParam MultipartFile file) {
+        AudioTranscriptionPrompt transcriptionPrompt =
+                new AudioTranscriptionPrompt(file.getResource());
+        AudioTranscriptionResponse response = audioModel.call(transcriptionPrompt);
+        return response
+                .getResult()
+                .getOutput();
     }
 
 }
