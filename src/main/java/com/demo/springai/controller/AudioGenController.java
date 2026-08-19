f1985fd611d79123909ea049048b62b3d272183f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-08-19 22:58:28 +0530

    audio speech model created

--- src/main/java/com/demo/springai/controller/AudioGenController.java
+++ src/main/java/com/demo/springai/controller/AudioGenController.java
@@ -3,6 +3,7 @@ package com.demo.springai.controller;
 import com.openai.models.audio.AudioResponseFormat;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,9 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 public class AudioGenController {
 
     private OpenAiAudioTranscriptionModel audioModel;
+    private OpenAiAudioSpeechModel audioSpeechModel;
 
-    public AudioGenController(OpenAiAudioTranscriptionModel audioModel) {
+    public AudioGenController(
+            OpenAiAudioTranscriptionModel audioModel
+            , OpenAiAudioSpeechModel audioSpeechModel
+    ) {
         this.audioModel = audioModel;
+        this.audioSpeechModel = audioSpeechModel;
     }
 
     @PostMapping("/api/stt")
